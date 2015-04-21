505b124273445d2e5ccbd474aba45a79a7a14c07
Bill Coon <coondoggie82@gmail.com>
2015-04-21 10:58:40 -0600

    Initial Commit with Blink Test

---
+++ Thing/Thing.cpp
@@ -0,0 +1,18 @@
+#include "Thing.h"
+
+
+//The setup function is called once at startup of the sketch
+void setup()
+{
+// Add your initialization code here
+	pinMode(13, OUTPUT);
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+	  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+	  delay(100);              // wait for a second
+	  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+	  delay(100);              // wait for a second
+}
---
+++ Thing/Thing.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef Thing_H_
+#define Thing_H_
+#include "Arduino.h"
+//add your includes for the project Thing here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project Thing here
+
+
+
+
+//Do not add code below this line
+#endif /* Thing_H_ */
