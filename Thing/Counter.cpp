ebbe4ab0286c15a1567735f949c4637d5fe9f9d1
Bill Coon <coondoggie82@gmail.com>
2015-04-22 12:23:50 -0600

    Add empty Counter class

---
+++ Thing/Counter.cpp
@@ -0,0 +1,18 @@
+/*
+ * Counter.cpp
+ *
+ *  Created on: Apr 22, 2015
+ *      Author: williamcoon
+ */
+
+#include <Counter.h>
+
+Counter::Counter() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Counter::~Counter() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Thing/Counter.h
@@ -0,0 +1,17 @@
+/*
+ * Counter.h
+ *
+ *  Created on: Apr 22, 2015
+ *      Author: williamcoon
+ */
+
+#ifndef COUNTER_H_
+#define COUNTER_H_
+
+class Counter {
+public:
+	Counter();
+	virtual ~Counter();
+};
+
+#endif /* COUNTER_H_ */
