61f0e539996264531e8dd31f05809adef9db1b39
Bill Coon <coondoggie82@gmail.com>
2015-07-14 17:11:00 -0600

    Reorganize and rename commands
    -Just to make things a bit easier to read and understand

--- Thing/Commands/CommandCreator.cpp
+++ Thing/Commands/CommandCreator.cpp
@@ -28,18 +28,16 @@ CommandCreator::CommandCreator() {
 	commandHash.put("eject", ejectBlock);
 	commandHash.put("reset", resetFingers);
 
-
 	//RFID tags
-	commandHash.put("770097AA4D07", rfidDrive1);
-	commandHash.put("770097F51207", rfidDrive2);
-	commandHash.put("78007C51A9FC", rfidDrive3);
-	commandHash.put("78007C7EA1DB", rfidDrive4);
+	commandHash.put("770097AA4D07", pointerFinger);
+	commandHash.put("770097F51207", reverse);
+	commandHash.put("78007C51A9FC", crawlForward);
+	commandHash.put("78007C7EA1DB", middleFinger);
 	commandHash.put("7600889AE783", resetFingers);
-	commandHash.put("770097BC5509", rfidDrive5);
+	commandHash.put("770097BC5509", peaceSign);
 	commandHash.put("770097AADA90", turnLeft);
 	commandHash.put("78007C900B9F", turnRight);
-	commandHash.put("78007C56E2B0", loveSign);
-	//commandHash.put("77000EBE33F4", shocker);
+	commandHash.put("78007C56E2B0", metalSign);
 	commandHash.put("78007C716F1A", highFive);
 	commandHash.put("78007C64CCAC", driveForward);
 	commandHash.put("78007C649BFB", driveForwardThirty);
@@ -99,6 +97,221 @@ Parameters* CommandCreator::parseParameters(String paramString){
 	}while(commaIndex>=0);
 	return params;
 }
+
+/********************************************
+ * Gesture Commands
+ ********************************************/
+
+/*
+ * pointerFinger(NULL)
+ * extends just the index finger
+ */
+bool CommandCreator::pointerFinger(Parameters *params){
+	Serial.println("Pointer Finger");
+	CommandBase *handCommand = new MoveHand(0,0,0,-13,0);
+	commandCenter->addCommand(handCommand);
+	ejectBlock(NULL);
+	return true;
+}
+
+/*
+ * crawlForward(NULL)
+ * Crawl forward. TODO: test this out and make it actually work
+ */
+bool CommandCreator::crawlForward(Parameters *params){
+	Serial.println("Crawl Forward");
+	CommandBase *wiggleCommand;
+	CommandBase *drive;
+	CommandBase *handUp;
+	handUp = new MoveHand(-12,-14,-13,-13,-5, 0);
+	commandCenter->addCommand(handUp);
+	for(int i=0;i<3;i++){
+		wiggleCommand = new WiggleFingers(-12,0,-5,0,300,3000,1);
+		wiggleCommand->setParallel(true);
+		commandCenter->addCommand(wiggleCommand);
+		drive = new Drive(driveSpeed, driveSpeed, 3);
+		commandCenter->addCommand(drive);
+		handUp = new MoveHand(-12,-14,-13,-13,-5, 0);
+		commandCenter->addCommand(handUp);
+	}
+	resetFingers(NULL);
+	ejectBlock(NULL);
+	return true;
+}
+
+/*
+ * middleFinger(NULL)
+ * I'm sure you can guess what this does.
+ */
+bool CommandCreator::middleFinger(Parameters *params){
+	Serial.println("Middle Finger");
+	CommandBase *handCommand = new MoveHand(0,0,-13,0,0);
+	commandCenter->addCommand(handCommand);
+	ejectBlock(NULL);
+	return true;
+}
+
+/*
+ * peaceSign(NULL)
+ * Makes a peace sign
+ */
+bool CommandCreator::peaceSign(Parameters *params){
+	Serial.println("Peace");
+	CommandBase *handCommand = new MoveHand(0,0,-13,-13,0);
+	commandCenter->addCommand(handCommand);
+	ejectBlock(NULL);
+	return true;
+}
+
+/*
+ * metalSign(NULL)
+ * Makes the metal sign
+ */
+bool CommandCreator::metalSign(Parameters *params){
+	Serial.println("Metal");
+	CommandBase *handCommand = new MoveHand(-12,0,0,-13,0);
+	commandCenter->addCommand(handCommand);
+	ejectBlock(NULL);
+	return true;
+}
+
+/*
+ * highFive(NULL)
+ * Puts all fingers out for a "High Five"
+ */
+bool CommandCreator::highFive(Parameters *params){
+	Serial.println("High Five");
+	CommandBase *handCommand = new MoveHand(-12,-14,-13,-13,-5);
+	commandCenter->addCommand(handCommand);
+	ejectBlock(NULL);
+	return true;
+}
+
+/********************************************
+ * Special Hand Commands
+ ********************************************/
+
+/*
+ * wiggleFingers(int minPosition, int maxPosition, int thumbMin, int thumbMax, int fingerDelay, int cycleDelay, int repeats)
+ */
+bool CommandCreator::wiggleFingers(Parameters *params){
+	int minPosition;
+	int maxPosition;
+	int thumbMin;
+	int thumbMax;
+	int fingerDelay;
+	int cycleDelay;
+	int repeats;
+	if(params->getInt(0, &minPosition)
+			&&params->getInt(1, &maxPosition)
+			&&params->getInt(2, &thumbMin)
+			&&params->getInt(3, &thumbMax)
+			&&params->getInt(4, &fingerDelay)
+			&&params->getInt(5, &cycleDelay)
+			&&params->getInt(6, &repeats))
+	{
+		CommandBase *command = new WiggleFingers(minPosition,
+												maxPosition,
+												thumbMin,
+												thumbMax,
+												fingerDelay,
+												cycleDelay,
+												repeats);
+		commandCenter->addCommand(command);
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool CommandCreator::resetFingers(Parameters *params){
+	Serial.println("Reset");
+	CommandBase *command = new FindHome(0); //This tells the hand to run to reed limits
+	commandCenter->addCommand(command);
+	CommandBase *command2 = new MoveHand(20,20,24,24,15,0);
+	commandCenter->addCommand(command2);
+	CommandBase *reset = new FindHome(-1); //This actually sets the home position
+	commandCenter->addCommand(reset);
+	ejectBlock(NULL);
+	return true;
+}
+
+/*
+ * findHome(int speed)
+ * calibrates the home position using the reed switches
+ */
+bool CommandCreator::findHome(Parameters *params){
+	int speed;
+	if(params->getInt(0, &speed)){
+		CommandBase *command = new FindHome(speed);
+		commandCenter->addCommand(command);
+		return true;
+	}else{
+		return false;
+	}
+}
+
+/********************************************
+ * Driving Commands
+ ********************************************/
+
+/*
+ * drive(int leftSpeed, int rightSpeed, int driveTime)
+ * leftSpeed: Integer from -100 to 100
+ * rightSpeed: Integer from -100 to 100
+ * driveTime: Seconds to drive before stopping. Use -1 to set indefinitely.
+ */
+
+bool CommandCreator::driveForward(Parameters *params){
+	Serial.println("Drive Forward");
+	CommandBase *command = new Drive(driveSpeed, driveSpeed, -1);
+	commandCenter->addCommand(command);
+	ejectBlock(NULL);
+	return true;
+}
+
+bool CommandCreator::driveForwardThirty(Parameters *params){
+	Serial.println("Drive Forward");
+	CommandBase *command = new Drive(driveSpeed, driveSpeed, 30);
+	commandCenter->addCommand(command);
+	ejectBlock(NULL);
+	return true;
+}
+
+
+bool CommandCreator::turnLeft(Parameters *params){
+	Serial.println("Turn left");
+	CommandBase *command = new Drive(20, 80, 4);
+	commandCenter->addCommand(command);
+	ejectBlock(NULL);
+	return true;
+}
+
+bool CommandCreator::turnRight(Parameters *params){
+	Serial.println("Turn right");
+	CommandBase *command = new Drive(70, 20, 4);
+	commandCenter->addCommand(command);
+	ejectBlock(NULL);
+	return true;
+}
+
+/*
+ * reverse(NULL)
+ * run in reverse for ten seconds
+ */
+bool CommandCreator::reverse(Parameters *params){
+	Serial.println("Reversing");
+	CommandBase *handCommand = new MoveHand(0,0,0,0,0,0); //Do we need to close the hand first?
+	commandCenter->addCommand(handCommand);
+	CommandBase *command = new Drive(-40, -40, 10);
+	commandCenter->addCommand(command);
+	ejectBlock(NULL);
+	return true;
+}
+
+/********************************************
+ * Debugging Commands - called from serial
+ ********************************************/
 
 /*
  * printSomething(String str, int numberOfRepeats)
@@ -175,12 +388,6 @@ bool CommandCreator::moveHand(Parameters *params){
 	}
 }
 
-/*
- * drive(int leftSpeed, int rightSpeed, int driveTime)
- * leftSpeed: Integer from -100 to 100
- * rightSpeed: Integer from -100 to 100
- * driveTime: Seconds to drive before stopping. Use -1 to set indefinitely.
- */
 bool CommandCreator::drive(Parameters *params){
 	int leftSpeed;
 	int rightSpeed;
@@ -194,222 +401,12 @@ bool CommandCreator::drive(Parameters *params){
 	}
 }
 
-/*
- * rfidDrive1(NULL)
- * No Parameters, this responds to an rfid tag and drives forward full speed for 3 seconds
- */
-bool CommandCreator::rfidDrive1(Parameters *params){
-	Serial.println("RFID Drive 1");
-	CommandBase *handCommand = new MoveHand(0,0,0,-13,0);
-	commandCenter->addCommand(handCommand);
-//	CommandBase *command = new Drive(driveSpeed, driveSpeed, 30);
-//	commandCenter->addCommand(command);
-//	CommandBase *handCommand2 = new MoveHand(0,0,0,0,0);
-//	commandCenter->addCommand(handCommand2);
-	ejectBlock(NULL);
-	return true;
-}
-
-/*
- * rfidDrive2(NULL)
- * No Parameters, this responds to an rfid tag and drives reverse full speed for 3 seconds
- */
-bool CommandCreator::rfidDrive2(Parameters *params){
-	Serial.println("RFID Drive 2");
-	CommandBase *handCommand = new MoveHand(0,0,0,0,0,0);
-	commandCenter->addCommand(handCommand);
-	CommandBase *command = new Drive(-40, -40, 10);
-	commandCenter->addCommand(command);
-	ejectBlock(NULL);
-	return true;
-}
-
-/*
- * rfidDrive3(NULL)
- * No Parameters, this responds to an rfid tag and spins full speed for 3 seconds
- */
-bool CommandCreator::rfidDrive3(Parameters *params){
-	Serial.println("RFID Drive 3");
-	CommandBase *wiggleCommand;
-	CommandBase *drive;
-	CommandBase *handUp;
-	handUp = new MoveHand(-12,-14,-13,-13,-5, 0);
-	commandCenter->addCommand(handUp);
-	for(int i=0;i<3;i++){
-		wiggleCommand = new WiggleFingers(-12,0,-5,0,300,3000,1);
-		wiggleCommand->setParallel(true);
-		commandCenter->addCommand(wiggleCommand);
-		drive = new Drive(driveSpeed, driveSpeed, 3);
-		commandCenter->addCommand(drive);
-		handUp = new MoveHand(-12,-14,-13,-13,-5, 0);
-		commandCenter->addCommand(handUp);
-	}
-	resetFingers(NULL);
-	ejectBlock(NULL);
-	return true;
-}
-
-/*
- * rfidDrive1(NULL)
- * No Parameters, this responds to an rfid tag and drives forward full speed for 3 seconds
- */
-bool CommandCreator::rfidDrive4(Parameters *params){
-	Serial.println("RFID Drive 4");
-	CommandBase *handCommand = new MoveHand(0,0,-13,0,0);
-	commandCenter->addCommand(handCommand);
-//	CommandBase *command = new Drive(driveSpeed, driveSpeed, 30);
-//	commandCenter->addCommand(command);
-//	CommandBase *handCommand2 = new MoveHand(0,0,0,0,0);
-//	commandCenter->addCommand(handCommand2);
-	ejectBlock(NULL);
-	return true;
-}
-
-/*
- * rfidDrive1(NULL)
- * No Parameters, this responds to an rfid tag and drives forward full speed for 3 seconds
- */
-bool CommandCreator::rfidDrive5(Parameters *params){
-	Serial.println("RFID Drive 4");
-	CommandBase *handCommand = new MoveHand(0,0,-13,-13,0);
-	commandCenter->addCommand(handCommand);
-//	CommandBase *command = new Drive(driveSpeed, driveSpeed, 30);
-//	commandCenter->addCommand(command);
-//	CommandBase *handCommand2 = new MoveHand(0,0,0,0,0);
-//	commandCenter->addCommand(handCommand2);
-	ejectBlock(NULL);
-	return true;
-}
-
-bool CommandCreator::turnLeft(Parameters *params){
-	Serial.println("Turn left");
-	CommandBase *command = new Drive(20, 80, 4);
-	commandCenter->addCommand(command);
-	ejectBlock(NULL);
-	return true;
-}
-
-bool CommandCreator::turnRight(Parameters *params){
-	Serial.println("Turn right");
-	CommandBase *command = new Drive(70, 20, 4);
-	commandCenter->addCommand(command);
-	ejectBlock(NULL);
-	return true;
-}
-
-bool CommandCreator::loveSign(Parameters *params){
-	Serial.println("Love");
-	CommandBase *handCommand = new MoveHand(-12,0,0,-13,0);
-	commandCenter->addCommand(handCommand);
-//	CommandBase *command = new Drive(driveSpeed, driveSpeed, 30);
-//	commandCenter->addCommand(command);
-//	CommandBase *handCommand2 = new MoveHand(0,0,0,0,0);
-//	commandCenter->addCommand(handCommand2);
-	ejectBlock(NULL);
-	return true;
-}
-
-bool CommandCreator::shocker(Parameters *params){
-	Serial.println("Shocker");
-	CommandBase *handCommand = new MoveHand(-12,0,-13,-13,0);
-	commandCenter->addCommand(handCommand);
-//	CommandBase *command = new Drive(driveSpeed, driveSpeed, 30);
-//	commandCenter->addCommand(command);
-//	CommandBase *handCommand2 = new MoveHand(0,0,0,0,0);
-//	commandCenter->addCommand(handCommand2);
-	ejectBlock(NULL);
-	return true;
-}
-
-bool CommandCreator::highFive(Parameters *params){
-	Serial.println("High Five");
-	CommandBase *handCommand = new MoveHand(-12,-14,-13,-13,-5);
-	commandCenter->addCommand(handCommand);
-//	CommandBase *command = new Drive(driveSpeed, driveSpeed, 30);
-//	commandCenter->addCommand(command);
-//	CommandBase *handCommand2 = new MoveHand(0,0,0,0,0);
-//	commandCenter->addCommand(handCommand2);
-	ejectBlock(NULL);
-	return true;
-}
-/*
- * calibrateVictor(int pin)
- * pin: the control pin for the victor
- * This command bypasses the command center and just executes here. It shouldn't
- * be used during normal operation and will screw up any currently running commands.
- * Before running this command, power up the victor and hold the "Cal" button
- * down until it starts alternating RED/GREEN. Release the button after you see
- * "Calibration Complete" printed to the console. The victor should then blink
- * green to confirm successful calibration.
- */
-bool CommandCreator::calibrateVictor(Parameters *params){
-	int pin;
-	if(params->getInt(0,&pin)){
-		Victor victor(pin);
-		victor.startServo();
-		Serial.println("Calibrating...");
-		victor.calibrate();
-		Serial.println("Calibration Complete");
-		return true;
-	}else{
-		return false;
-	}
-}
-
 bool CommandCreator::backAndForth(Parameters *params){
 	int fingerIndex;
 	int speed;
 	int cycles;
 	if(params->getInt(0, &fingerIndex)&&params->getInt(1, &speed)&&params->getInt(2, &cycles)){
 		CommandBase *command = new BackAndForth(fingerIndex, speed, cycles);
-		commandCenter->addCommand(command);
-		return true;
-	}else{
-		return false;
-	}
-}
-
-/*
- * findHome(int speed)
- * On startup, this calibrates the home position using the reed switches
- */
-bool CommandCreator::findHome(Parameters *params){
-	int speed;
-	if(params->getInt(0, &speed)){
-		CommandBase *command = new FindHome(speed);
-		commandCenter->addCommand(command);
-		return true;
-	}else{
-		return false;
-	}
-}
-
-/*
- * wiggleFingers(int minPosition, int maxPosition, int thumbMin, int thumbMax, int fingerDelay, int cycleDelay, int repeats)
- */
-bool CommandCreator::wiggleFingers(Parameters *params){
-	int minPosition;
-	int maxPosition;
-	int thumbMin;
-	int thumbMax;
-	int fingerDelay;
-	int cycleDelay;
-	int repeats;
-	if(params->getInt(0, &minPosition)
-			&&params->getInt(1, &maxPosition)
-			&&params->getInt(2, &thumbMin)
-			&&params->getInt(3, &thumbMax)
-			&&params->getInt(4, &fingerDelay)
-			&&params->getInt(5, &cycleDelay)
-			&&params->getInt(6, &repeats))
-	{
-		CommandBase *command = new WiggleFingers(minPosition,
-												maxPosition,
-												thumbMin,
-												thumbMax,
-												fingerDelay,
-												cycleDelay,
-												repeats);
 		commandCenter->addCommand(command);
 		return true;
 	}else{
@@ -442,6 +439,34 @@ bool CommandCreator::testParallel(Parameters *params){
 	return true;
 }
 
+/********************************************
+ * Other Commands
+ ********************************************/
+
+/*
+ * calibrateVictor(int pin)
+ * pin: the control pin for the victor
+ * This command bypasses the command center and just executes here. It shouldn't
+ * be used during normal operation and will screw up any currently running commands.
+ * Before running this command, power up the victor and hold the "Cal" button
+ * down until it starts alternating RED/GREEN. Release the button after you see
+ * "Calibration Complete" printed to the console. The victor should then blink
+ * green to confirm successful calibration.
+ */
+bool CommandCreator::calibrateVictor(Parameters *params){
+	int pin;
+	if(params->getInt(0,&pin)){
+		Victor victor(pin);
+		victor.startServo();
+		Serial.println("Calibrating...");
+		victor.calibrate();
+		Serial.println("Calibration Complete");
+		return true;
+	}else{
+		return false;
+	}
+}
+
 /*
  * smallPoof()
  * A 100 millisecond pulse to the poofer solenoid
@@ -463,33 +488,4 @@ bool CommandCreator::ejectBlock(Parameters *params){
 	commandCenter->addCommand(ejectCommand);
 	return true;
 }
-
-bool CommandCreator::resetFingers(Parameters *params){
-	Serial.println("Reset");
-	CommandBase *command = new FindHome(0); //This tells the hand to run to reed limits
-	commandCenter->addCommand(command);
-	CommandBase *command2 = new MoveHand(20,20,24,24,15,0);
-	commandCenter->addCommand(command2);
-	CommandBase *reset = new FindHome(-1); //This actually sets the home position
-	commandCenter->addCommand(reset);
-	ejectBlock(NULL);
-	return true;
-}
-
-bool CommandCreator::driveForward(Parameters *params){
-	Serial.println("Drive Forward");
-	CommandBase *command = new Drive(driveSpeed, driveSpeed, -1);
-	commandCenter->addCommand(command);
-	ejectBlock(NULL);
-	return true;
-}
-
-bool CommandCreator::driveForwardThirty(Parameters *params){
-	Serial.println("Drive Forward");
-	CommandBase *command = new Drive(driveSpeed, driveSpeed, 30);
-	commandCenter->addCommand(command);
-	ejectBlock(NULL);
-	return true;
-}
-//<moveHand(-12,-14,-13,-13,-5)>
 
